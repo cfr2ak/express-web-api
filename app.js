049c81167bc3233c51339f3270e3db26a27bcd5e
cfr2ak <cfr2ak@gmail.com>
2019-04-26 14:54:37 +0900

    favicon setting router setting

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+// const favicon = require('serve-favicon');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// const catalogRoutes = require('./routes/catalog');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+// app.use(favicon(path.join(__dirnanem, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/catalog', catalogRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -28,7 +26,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
